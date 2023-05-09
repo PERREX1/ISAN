7f9a637073029d29c552466b8c7429073647b33c
David_Quintero <perrex17@gmail.com>
2023-05-09 17:57:20 -0500

    problem footer

--- src/res/js/mod.js
+++ src/res/js/mod.js
@@ -11,3 +11,23 @@ window.addEventListener('scroll', function() {
       header.classList.remove('header-fixed');
     }
   });
+
+  //las imagenes que aparezcan 
+
+  window.addEventListener('scroll', function() {
+    var textSection = document.querySelector('.texto');
+    var imageSection = document.querySelector('.imagenes');
+    var scrollPosition = window.scrollY;
+    var windowHeight = window.innerHeight;
+  
+    var textSectionHeight = textSection.offsetHeight;
+    var imageSectionHeight = imageSection.offsetHeight;
+  
+    if (scrollPosition > textSectionHeight - windowHeight / 2) {
+      imageSection.classList.add('img-visible');
+      imageSection.classList.remove('img-hidden');
+    } else {
+      imageSection.classList.add('img-hidden');
+      imageSection.classList.remove('img-visible');
+    }
+  });
